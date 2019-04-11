21543693f138af5ba9d1c0ef361684219cb069b6
jsxgod <Smykoo7@gmail.com>
2019-04-11 20:36:05 +0200

    Initial Project Commit

---
+++ src/main/java/js/springexamples/jspetclinic/JsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package js.springexamples.jspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsPetClinicApplication.class, args);
+    }
+
+}
