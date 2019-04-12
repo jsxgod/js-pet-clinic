48d076986bff7afff9c6520eb13bc11dbd40c052
jsxgod <Smykoo7@gmail.com>
2019-04-12 20:49:25 +0200

    Adding base entity. closes #10

--- pet-clinic-data/src/main/java/js/springexamples/jspetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/js/springexamples/jspetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package js.springexamples.jspetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
