f0456ce06d89b48119c8890fddb21c08cb813759
jsxgod <Smykoo7@gmail.com>
2019-04-13 00:33:37 +0200

    Adding vet index page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/js/springexamples/jspetclinic/controllers/VetIndexController.java
@@ -0,0 +1,14 @@
+package js.springexamples.jspetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetIndexController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
