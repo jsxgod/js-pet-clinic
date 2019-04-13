2803bef4e3fb1add70882982b5b5a30e3d069f98
jsxgod <Smykoo7@gmail.com>
2019-04-13 08:49:25 +0200

    Adding Speciality Service Interface and Map Service Implementation. closes #31

---
+++ pet-clinic-data/src/main/java/js/springexamples/jspetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package js.springexamples.jspetclinic.services;
+
+import js.springexamples.jspetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
