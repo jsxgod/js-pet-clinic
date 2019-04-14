426e5ca8991cd425b0a264861ca5b5cebe8379a6
jsxgod <Smykoo7@gmail.com>
2019-04-14 22:48:51 +0200

    Implementing map service for Visit. closes #46

---
+++ pet-clinic-data/src/main/java/js/springexamples/jspetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package js.springexamples.jspetclinic.services;
+
+import js.springexamples.jspetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/js/springexamples/jspetclinic/services/map/VisitMapService.java
@@ -0,0 +1,48 @@
+package js.springexamples.jspetclinic.services.map;
+
+import js.springexamples.jspetclinic.model.Visit;
+import js.springexamples.jspetclinic.repositories.VisitRepository;
+import js.springexamples.jspetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitMapService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+       return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null || object.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
